a8472fc603fe3a3661b9754f9237ae9452d0b823
Tharin Sae-ung <tharins@hotmail.co.th>
2024-12-12 00:32:53 +0700

    changed root path to src

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
 
     config.resolve.alias = {
 
       ...config.resolve.alias,
 
-      '@': path.resolve(__dirname, ''),
+      '@': path.resolve(__dirname, 'src'),
 
     };
 
